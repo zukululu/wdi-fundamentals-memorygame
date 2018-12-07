7255c0cad13ecf403f878c052ed66607ffa64e09
Jeff Duong <duong.jefferson@gmail.com>
2018-12-07 00:21:21 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
-console.log("Up and running!");
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
 
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'king';
+cardsInPlay.push(cardOne);
+console.log('User flipped queen');
+cardsInPlay.push(cardTwo);
+console.log('User flipped king');
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+if(cardsInPlay.length === 2) 
+{
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert('Sorry, try again.'); }
+}
